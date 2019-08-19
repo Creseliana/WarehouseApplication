6ecbe48e52e45de31621a72992c508021bbf47df
Creselia <valeria.sterzhanova@gmail.com>
2019-08-20 00:49:48 +0300

    Initial commit. Login page created. User's classes added.

---
+++ WarehouseApplication/Pages/LoginPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WarehouseApplication.Users;
+
+namespace WarehouseApplication.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        private string inputLogin;
+        private string inputPassword;
+        private AbstractUser user;
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_TextChanged(object sender, TextChangedEventArgs e)
+        {           
+        }
+
+        private void Password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            Password.Clear();
+            NavigationService.Navigate(new Page1());
+        }
+    }
+}
---
+++ WarehouseApplication/Users/AbstractUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseApplication.Users
+{
+    abstract class AbstractUser
+    {
+        public const int ADMIN = 1;
+        public const int MANAGER = 2;
+        public const int USER = 3;
+
+        public int Id { get; set; }
+        public string Login { get; set; }
+        private string password;
+        public string Password //encrypt decrypt
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                this.password = value;
+            }
+        }
+        public int Role { get; set; }
+        public string FullName { get; set; }
+    }
+}
